83e6d375c570a4e7b04327a2cd34572558ea7061
vimen99 <79522793+vimen99@users.noreply.github.com>
2021-07-24 17:31:18 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay HELLO VIMEN
+echo $green"selamat datang"
+echo
+sleep 2
+echo $yellow"silahkan pilih toolsnya :"
+echo
+echo $cyan"1.) stabilkan jaringan"
+echo
+echo $blue"0.) keluar aja !!!"
+echo
+echo $white
+read -p "pilih mana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay VIMEN
+echo $green"stabilkan jaringan by VIMEN"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "HELLO VIMEN"
+echo $white"To Be Continued"
+sleep3
+exit
+fi
